f28db29cdba92599440ab89d8a0240483783bafd
xiezhechao <xiezhechao@hotmail.com>
2017-09-05 12:08:36 +0800

    '取消跟踪配置文件'

--- app/Home/Conf/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-    'ADMINISTRATOR'        =>  'admin',    //session里有这个管理员不需要认证
-);
--- app/Home/Conf/tag.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: 谢哲超
- * Date: 15/11/6
- * Time: 01:46
- */
